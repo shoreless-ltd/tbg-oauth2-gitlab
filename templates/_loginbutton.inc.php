ee39b1534770d3fca9bfae13f4b7d29bca61fb71
Mario Steinitz <mario.steinitz@shoreless.limited>
2017-06-30 19:23:10 +0800

    Fix styling of login button. Update composer dependencies.

--- templates/_loginbutton.inc.php
+++ templates/_loginbutton.inc.php
@@ -1,6 +1,13 @@
 <style>
+    #oauth2-gitlab-signin-button.persona-button span::before {
+        background: #fff;
+    }
     #oauth2-gitlab-signin-button.persona-button span::after {
         background-image : url('<?= image_url('gitlab-normal.png', false, 'oauth2_gitlab'); ?>');
+        width: 40px;
+    }
+    #oauth2-gitlab-signin-button.persona-button::before {
+        background: #fff;
     }
 </style>
 <div class="logindiv openid_container" id="oauth2_gitlab_openid_container" style="display: block;">
@@ -10,6 +17,6 @@
             <legend style="text-align: center; width: 100%; background-color: transparent;"><?php echo __('%regular_login or %persona_or_openid_login', array('%regular_login' => '', '%persona_or_openid_login' => '')); ?></legend>
         </fieldset>
         <?php endif; ?>
-        <a class="persona-button large blue" id="oauth2-gitlab-signin-button" href="<?= make_url('oauth2_gitlab_login'); ?>"><span><?php echo empty($gitlab_btn_label) ? __('Use GitLab') : $gitlab_btn_label; ?></span></a>
+        <a class="persona-button large orange" id="oauth2-gitlab-signin-button" href="<?= make_url('oauth2_gitlab_login'); ?>"><span><?php echo empty($gitlab_btn_label) ? __('Use GitLab') : $gitlab_btn_label; ?></span></a>
     </div>
 </div>
